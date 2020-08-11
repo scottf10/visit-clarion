dbcba69701035775d284fa89ab8a5aa66fb3180e
Scott Florence Jr <scott10florence@gmail.com>
2020-08-11 10:45:17 -0400

    add existing file

--- src/templates/stay-page.js
+++ src/templates/stay-page.js
@@ -4,8 +4,8 @@ import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Features from '../components/Features'
 import Testimonials from '../components/Testimonials'
-import Pricing from '../components/Pricing'
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
+import StayRoll from '../components/StayRoll'
 
 export const StayPageTemplate = ({
   image,
@@ -16,31 +16,91 @@ export const StayPageTemplate = ({
   main,
   testimonials,
   fullImage,
-  pricing,
 }) => (
-  <div className="content">
-    <div
-      className="full-width-image-container margin-top-0"
-      style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-      }}
-    >
-      <h2
-        className="has-text-weight-bold is-size-1"
+    <div className="content">
+      <div
+        className="full-width-image-container margin-top-0"
         style={{
-          boxShadow: '0.5rem 0 0 #df9508, -0.5rem 0 0 #df9508',
-          backgroundColor: '#df9508',
-          color: 'white',
-          padding: '1rem',
+          backgroundImage: `url(${
+            !!image.childImageSharp ? image.childImageSharp.fluid.src : image
+            })`,
         }}
       >
-        {title}
-      </h2>
+        <h2
+          className="has-text-weight-bold is-size-1"
+          style={{
+            boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
+            backgroundColor: '#f40',
+            color: 'white',
+            padding: '1rem',
+          }}
+        >
+          {title}
+        </h2>
+      </div>
+      <section className="section section--gradient">
+        <div className="container">
+          <div className="section">
+            <div className="columns">
+              <div className="column is-7 is-offset-1">
+                <h3 className="has-text-weight-semibold is-size-2">{heading}</h3>
+                <p>{description}</p>
+              </div>
+            </div>
+
+            <div className="tile is-ancestor">
+              <div className="tile is-vertical">
+                <div className="tile">
+                  <div className="tile is-parent is-vertical">
+                    <article className="tile is-child">
+                      <PreviewCompatibleImage imageInfo={main.image1} />
+                    </article>
+                  </div>
+                  <div className="tile is-parent">
+                    <article className="tile is-child">
+                      <PreviewCompatibleImage imageInfo={main.image2} />
+                    </article>
+                  </div>
+                </div>
+                <div className="tile is-parent">
+                  <article className="tile is-child">
+                    <PreviewCompatibleImage imageInfo={main.image3} />
+                  </article>
+                </div>
+              </div>
+            </div>
+            <Testimonials testimonials={testimonials} />
+            <div
+              className="full-width-image-container"
+              style={{
+                backgroundImage: `url(${
+                  fullImage.childImageSharp
+                    ? fullImage.childImageSharp.fluid.src
+                    : fullImage
+                  })`,
+              }}
+            />
+
+            <section className="section">
+              <div className="container">
+                <div className="columns ">
+                  <div className="column is-7">
+                    <h3 className="has-text-weight-semibold is-size-3 margin-top-60">
+                      {main.heading}
+                    </h3>
+                  </div>
+                </div>
+                <div className="content">
+                  <StayRoll />
+                </div>
+              </div>
+            </section>
+
+          </div>
+        </div>
+      </section>
     </div>
-  </div>
-)
+  )
 
 StayPageTemplate.propTypes = {
   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
@@ -59,11 +119,6 @@ StayPageTemplate.propTypes = {
   }),
   testimonials: PropTypes.array,
   fullImage: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  pricing: PropTypes.shape({
-    heading: PropTypes.string,
-    description: PropTypes.string,
-    plans: PropTypes.array,
-  }),
 }
 
 const StayPage = ({ data }) => {
@@ -80,7 +135,6 @@ const StayPage = ({ data }) => {
         main={frontmatter.main}
         testimonials={frontmatter.testimonials}
         fullImage={frontmatter.full_image}
-        pricing={frontmatter.pricing}
       />
     </Layout>
   )
@@ -167,16 +221,6 @@ export const stayPageQuery = graphql`
             fluid(maxWidth: 2048, quality: 100) {
               ...GatsbyImageSharpFluid
             }
-          }
-        }
-        pricing {
-          heading
-          description
-          plans {
-            description
-            items
-            plan
-            price
           }
         }
       }
